6e7538653d8f6f3cbacd713a2a8d5ae6befbdad6
Tomasz Kowalewski <me@tkowalewski.pl>
2023-12-29 19:22:03 +0100

    Update assets manifest

--- app/assets/config/hello_world_manifest.js
+++ app/assets/config/hello_world_manifest.js
@@ -1,3 +1,4 @@
 //= link_tree ../javascripts/hello_world .js
+//= link_tree ../../components/hello_world .js
 //= link_directory ../stylesheets/hello_world .css
 //= link_tree ../builds/hello_world .css
