7ef3095b48acc942dba30ac3af3b5f05b9ef58c9
Tomasz Kowalewski <me@tkowalewski.pl>
2023-12-29 19:19:25 +0100

    Example component will render hello message

--- app/components/hello_world/example_component_controller.js
+++ app/components/hello_world/example_component_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   connect() {
     console.log("Hello, Stimulus!", this.element);
+    this.element.textContent = "Hello, Stimulus!"
   }
 }
